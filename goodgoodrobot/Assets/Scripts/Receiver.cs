6f3309d2fffb45247d4cdd0ac9050fd2504cd816
corianderd <corianderd@tiltyhouse.com>
2017-05-20 11:46:23 -0700

    Button sounds

---
+++ goodgoodrobot/Assets/Scripts/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using s3DBakers.Buttons;
+
+public class Receiver : s3DBButton_receiver {
+
+	[SerializeField] AudioSource audio;
+	[SerializeField] AudioClip[] clips;
+
+public void button (str3DBbMessage msg) {
+		base.button (msg);
+
+		AudioClip playClip;
+		if (clips.Length > 0) {
+			playClip = clips [0];
+		} else {
+			playClip = audio.clip;
+		}
+
+		if (audio != null && playClip != null) {
+			audio.PlayOneShot (playClip);
+		}
+}
+
+}
