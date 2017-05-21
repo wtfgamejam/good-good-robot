d7e1c1051c29634f5e9f6d78c659462eeacaa0e2
Sara Chicazul <sara.chicazul@gmail.com>
2017-05-21 11:44:54 -0700

    Add new button test scene & blinky lights

---
+++ goodgoodrobot/Assets/Scripts/Blink.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blink : MonoBehaviour
+{
+	public Color baseColor;
+	public float Speed = 1;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Renderer renderer = GetComponent<Renderer> ();
+		Material mat = renderer.material;
+
+		float emission = Mathf.PingPong (Time.time * Speed, 1.0f);
+		Color finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
+
+		mat.SetColor ("_EmissionColor", finalColor);
+	}
+}
+
