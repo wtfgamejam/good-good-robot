a23d1925ec3f55d756540d1bd56915b5f18a7e4f
corianderd <corianderd@tiltyhouse.com>
2017-05-20 20:00:18 -0700

    Motion curves for the button stuff

---
+++ goodgoodrobot/Assets/Scripts/MotionCurve.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MotionCurve {
+
+	public AnimationCurve motionCurve = AnimationCurve.Linear(0, 0, 1, 1);
+	public Vector3 start = Vector3.zero;
+	public Vector3 end = Vector3.zero;
+	public float motionDuration = 1.0f;
+
+	public void SetStart(Vector3 s)
+	{
+		start = s;
+	}
+
+	public void SetEnd(Vector3 e)
+	{
+		end = e;
+	}
+
+	public Vector3 GetValue(float percent)
+	{
+		float curvedValue = motionCurve.Evaluate(percent);
+		Vector3 newPos = Vector3.Lerp (start, end, curvedValue);
+		return newPos;
+	}
+
+}
--- goodgoodrobot/Assets/Scripts/Receiver.cs
+++ goodgoodrobot/Assets/Scripts/Receiver.cs
@@ -8,8 +8,12 @@ public class Receiver : s3DBButton_receiver {
 	[SerializeField] AudioSource audio;
 	[SerializeField] AudioClip[] clips;
 
+	public MotionCurve[] motions;
+	private int currentMotion = 0;
+	private IEnumerator motionCoroutine;
+
 public void button (str3DBbMessage msg) {
-		base.button (msg);
+		//base.button (msg);
 
 		AudioClip playClip;
 		if (clips.Length > 0) {
@@ -21,6 +25,39 @@ public void button (str3DBbMessage msg) {
 		if (audio != null && playClip != null) {
 			audio.PlayOneShot (playClip);
 		}
+
+		if (msg.actions.position) {
+			if (motionCoroutine != null) {
+				StopCoroutine (motionCoroutine);
+			}
+			motionCoroutine = Movement (motions[currentMotion]);
+			StartCoroutine (motionCoroutine);
+		}
 }
+		
+	IEnumerator Movement(MotionCurve motion)
+	{
+		transform.localPosition = motion.start;
+		float duration = motion.motionDuration;
+		float elapsed = 0;
+
+		while (elapsed < duration) {
+			elapsed += Time.deltaTime;
+			transform.localPosition = motion.GetValue (elapsed / duration);
+			yield return null;
+		}
+
+		EndMotion (motion);
+		yield return null;
+	}
+
+	void EndMotion(MotionCurve motion)
+	{
+		transform.localPosition = motion.end;
+		currentMotion++;
+		if (currentMotion > motions.Length - 1) {
+			currentMotion = 0;
+		}
+	}
 
 }
