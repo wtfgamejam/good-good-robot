dbf8c82ed7a311cbb0eb5145e0e238cdf46c5f55
corianderd <corianderd@tiltyhouse.com>
2017-05-22 11:05:48 -0700

    add some lists of names for objects

---
+++ goodgoodrobot/Assets/Scripts/ShipSystems.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is just a data class for holding names
+public class ShipSystems {
+
+
+	// Life Support
+	public static string[] lifeSupportSystems = { "Defibrillator", "Ventilation", "Feeding Tube", "Spome", "Oxygen Purge System", "Suit Telemetry", "Odor Remover", "Air-like mixture", "Toilets", "Tea", "Oxygen", "Carbon Scrubbers", "Water Filtration", "Temperature Control" };
+    
+	// Engines
+	public static string[] engineSystems = { "Fullerenes", "Speed Brake", "Fuel Injection", "Thrusters", "Pyrotechnic Fasteners", "Main Engine", "Coolant", "Thermal Exhaust", "Main Exhaust", "Minor Exhaust" };
+
+	// Sensors
+	public static string[] sensorSystem = { "Compound Eye", "Accelerometer", "Chronometer", "Barometer", "Hydrogen Detector", "Mass Spectrometer", "Medium Range Camera", "Proximity Detector", "Embedded System", "Photon Detector", "Grid Array", "Main Dish", "Forward Array", "Neutrino Detector" };
+    
+	// Piloting Systems
+	public static string[] flightSystem = { "Anti-Collision Lights", "Attitude Control", "Autopilot", "Orbital Maneuvering System" };
+}
